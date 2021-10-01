db1f12216c12fcd47552b9184d824f08995ce71c
edison.oda@hotmail.com <edison.oda@hotmail.com>
2021-09-30 22:04:50 -0300

    Add teste do crud

---
+++ test/crudTest.js
@@ -0,0 +1,26 @@
+const assert = require("chai").assert;
+const data = require("../veiculos.json");
+var VeiculoController = require("../Controllers/VeiculoController");
+
+describe("FindById", function() {
+    let id = data.veiculos[0].id;
+
+    it("Caso padrão", function() {
+        if(id){
+            let resultado = VeiculoController.findById(id);
+            assert.typeOf(resultado, "Object");
+        }else{
+            assert.isNull(id);
+        }
+    });
+
+    it("ID negativo", function() {
+        let resultado = VeiculoController.findById(-1);
+        assert.isNull(resultado);
+    });
+
+    it("ID como string", function() {
+        let resultado = VeiculoController.findById("teste");
+        assert.isNull(resultado);
+    });
+});
