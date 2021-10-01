d33fabce865802e67cc202755efe3bdd8483bd8f
edison.oda@hotmail.com <edison.oda@hotmail.com>
2021-09-30 22:03:09 -0300

    Add routes

---
+++ routes.js
@@ -0,0 +1,10 @@
+var VeiculoController = require("./Controllers/VeiculoController");
+const veiculoController = new VeiculoController();
+
+module.exports = function(server){
+    server.post("/", veiculoController.create);
+    server.put("/:id", veiculoController.update);
+    server.delete("/:id", veiculoController.delete);
+    server.get("/", veiculoController.index);
+    server.get("/:id", veiculoController.get);
+}
